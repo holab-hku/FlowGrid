25af67bbe5f5c36d3471b1fda3da05c3510632f2
XiunanFang <xfanuu@gmail.com>
2019-09-03 17:15:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'FlowGrid',         # How you named your package folder (MyLib)
   packages = ['FlowGrid'],   # Chose the same as "name"
-  version = '0.01',      # Start with a small number and increase it with every change you make
+  version = '0.10',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'FlowGrid implemented in Scanpy',   # Give a short description about your library
   author = 'Xiunan Fang',                   # Type in your name
