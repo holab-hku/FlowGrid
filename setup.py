1c528739c158bdfee8762c99bb95ab5396d9a05b
XiunanFang <xfanuu@gmail.com>
2019-09-05 16:08:40 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'FlowGrid',         
+  packages = ['FlowGrid'],   
+  version = 'v0.1',      
+  license='MIT',        
+  description = 'FlowGrid implemented in Scanpy',   
+  author = 'Xiunan Fang',                   
+  author_email = 'xiunan@hku.hk',      
+  url = 'https://github.com/holab_hku/FlowGrid',   
+  download_url = 'https://github.com/holab_hku/FlowGrid/archive/v_01.tar.gz',    
+  keywords = ['Single Cell RNA-seq', 'Scanpy', 'Clustering'],   
+  install_requires=[           
+          'numpy',
+          'pandas',
+          'anndata',
+          'natsort',
+          'typing',
+          'sklearn',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
