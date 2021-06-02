9c7682e6f5a563d9ec8a150ec3e6b8fd58f0a337
xfanuu <xfanuu@gmail.com>
2021-06-02 17:55:24 +0800

    fg

--- FlowGrid/_consensusFlowGrid.py
+++ FlowGrid/_consensusFlowGrid.py
@@ -141,4 +141,52 @@ def consensusFlowGrid(
             del adata.obs[label]
     print("ConsensusFlowGrid completed in : "+ str(round(time()-t0,3)) + " seconds.\n")
     
+bin_n = 15
+eps = 5
+MinDenB = 200
+MinDenC = 500 
+def cluster(
+    adata: AnnData,
+    MinDenB: Optional[int] = 200,
+    bin_n: int = bin_n,
+    eps: float = eps, 
+    MinDenC: Optional[int] = 500,
+    DimRMethod: Optional[str] = 'PCA',
+    copy: bool = False,
+    chunked: bool = False,
+    chunk_size: Optional[int] = None,    
+) -> Optional[AnnData]:
     
+    adata = adata.copy() if copy else adata
+    if DimRMethod == 'PCA':
+        projected_data = adata.obsm['X_pca'][:,0:5].astype(float)
+    if DimRMethod == 'UMAP':
+        projected_data = adata.obsm['X_umap'][:,0:5].astype(float)
+    if DimRMethod == 'DIFFMAP':
+        projected_data = adata.obsm["X_diffmap"][:,0:5].astype(float)
+    if DimRMethod == 'NMF':
+        projected_data = adata.obsm['X_nmf'][:,0:5].astype(float)
+    if DimRMethod == 'FA':
+        projected_data = adata.obsm['X_fa'][:,0:5].astype(float)   
+
+        
+    projected_data.shape[1]
+    n_dimension = 5
+    transposed_pdata =[]
+    for i in range(n_dimension):
+        positive_pdata = projected_data[:,i] - np.amin(projected_data[:,i])
+        transposed_pdata.append(positive_pdata)
+    shifted_pdata = np.array(transposed_pdata).transpose()
+    shifted_pdata = shifted_pdata.astype(float)
+
+    fg_object = FlowGrid(shifted_pdata, MinDenB = MinDenB, bin_n = bin_n, eps = math.sqrt(eps), MinDenC = MinDenC)
+    key_added = 'MinDenB_'+ str(MinDenB) + '_binN_' + str(bin_n) + '_eps_' + str(eps) + '_MinDenC_' + str(MinDenC) + '_FlowGrid'
+
+    flowgrid_label = fg_object.clustering()
+
+    adata.obs[key_added] = pd.Categorical(
+    values=flowgrid_label.astype('U'),
+    categories=natsorted(np.unique(flowgrid_label).astype('U')),
+    )
+
+    return adata if copy else None    
